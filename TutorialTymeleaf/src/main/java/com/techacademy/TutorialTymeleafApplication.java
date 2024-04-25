4b85d390d5c1fa3b778df97720c0c08a618ec7eb
yumeno.yoshizaki <yumeno.yoshizaki.v321.dirbato.co.jp>
2024-04-25 09:16:40 +0900

    lesson20

---
+++ TutorialTymeleaf/src/main/java/com/techacademy/TutorialTymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialTymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialTymeleafApplication.class, args);
+	}
+
+}
